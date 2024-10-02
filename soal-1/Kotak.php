01ac0a2bbca291b5fedc0bd0a4b4b3784c50ade2
NaisylaAnwar <23_naisylanwar@student.smkti.net>
2024-10-03 06:48:54 +0700

    selesai

--- soal-1/Kotak.php
+++ soal-1/Kotak.php
@@ -1,24 +1,34 @@
 <?php
 class PersegiPanjang
 {
+    public $panjang;
+    public $lebar;
     // deklarasikan panjang dan lebar
-
-
+    public function __construct($panjang, $lebar)
+    {
+        $this->panjang = $panjang;
+        $this->lebar = $lebar;
+    }
     // deklarasikan metode konstruktor
 
-
+    public function setPanjang($panjang)
+    {
+        $this->panjang = $panjang;
+    }
     // deklarasikan metode mutator dan asesor
-
-
-
+    {
+        $this->lebar = $lebar;
+    }
     public function hitungLuas()
     {
+        return $this->panjang * $this->lebar;
         // lengkapi kode untuk menghitung luas persegi panjang
 
     }
 
     public function hitungKeliling()
     {
+        return 2 * ($this->panjang + $this->lebar);
         // lengkapi dengan kode untuk menghitung keliling persegi panjang
 
     }
@@ -27,3 +37,5 @@ class PersegiPanjang
 $contoh = new PersegiPanjang(12, 20);
 echo "Luas Persegi Panjang: " . $contoh->hitungLuas() . "<br>";
 echo "Keliling Persegi Panjang: " . $contoh->hitungKeliling() . "<br>";
+
+?>
--- soal-3/Mobil.php
+++ soal-3/Mobil.php
@@ -1,5 +1,16 @@
 <?php
 class Mobil {
+        public $merek;
+        public $plat;
+        public $model;
+        public $warna;
+        public $tahun;
+        public $kecepatan = 0;
+    
+        public function Melaju() {
+            echo "Mobil Sedang Melaju Kencang.";
+        }
+    
     // lengkapi dengan atribut dan metode sesuai pada buku
 
     // contoh metode melaju()
